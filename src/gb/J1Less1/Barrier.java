e3310f270c1738e8c38f7863de69a964df15b6b2
Gaaronk <s.ivanov.kr23@gmail.com>
2021-10-01 12:26:51 +0500

    Initial Commit

---
+++ src/gb/J1Less1/Barrier.java
@@ -0,0 +1,21 @@
+package gb.J1Less1;
+
+public class Barrier
+{
+    private String name;
+
+    public Barrier(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
--- src/gb/J1Less1/Team.java
+++ src/gb/J1Less1/Team.java
@@ -5,7 +5,8 @@ public class Team
     String teamName;
     TeamMember[] members;
 
-    public Team(String teamName, TeamMember[] members) {
+    public Team(String teamName, TeamMember[] members)
+    {
         this.teamName = teamName;
         this.members = members;
     }
@@ -14,17 +15,17 @@ public class Team
     {
         for(int i =0; i< members.length;i++)
         {
-            System.out.println(members[i].memberName);
+            System.out.println(members[i].getMemberName());
         }
     }
 
-    void WhoPassedCourse(TeamMember[] members)
+    void WhoPassedCourse(TeamMember[] members, Course course)
     {
         for(int i =0; i< members.length;i++)
         {
-            if(members[i].coursePassed)
+            if(members[i].getNumOfBarrierPassed() == course.barriers.length)
             {
-                System.out.println(members[i].memberName);
+                System.out.println(members[i].getMemberName() + " passed course");
             }
         }
     }
